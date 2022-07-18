6c29b5aa2e02932fb3e802f619c537d87b95d6ca
Alienaaa <alien0417new@gmail.com>
2022-07-18 17:46:58 +0900

    new: topic owner header

---
+++ javascripts/discourse/components/ttl-topic-owner-header.js
@@ -0,0 +1,9 @@
+import Component from "@ember/component";
+// import discourseComputed from "discourse-common/utils/decorators";
+
+export default Component.extend({
+    // tagName: "div",
+    // classNames: ["ttl-nav-bar"],
+    // classNameBindings: ["isMarkerHidden"],
+
+});
