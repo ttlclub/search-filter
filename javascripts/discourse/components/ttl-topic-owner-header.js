1bd25c7a3f1f7199141a22220fa4243def3e3056
Alienaaa <alien0417new@gmail.com>
2022-07-20 15:14:02 +0900

    fix

--- javascripts/discourse/components/ttl-topic-owner-header.js
+++ javascripts/discourse/components/ttl-topic-owner-header.js
@@ -4,4 +4,9 @@ import { alias } from "@ember/object/computed";
 export default Component.extend({
     firstPost: alias("topic.postStream.posts.0"),
     firstPostUser: alias("topic.posters.0.user"),
+    // testUser: this.creator(),
+
+    didInsertElement() {
+        // console.log(this.testUser);
+    },
 });
