d1c433c3281a73cf67529ec5e07fd4ffc6364d02
Alienaaa <alien0417new@gmail.com>
2022-07-24 18:25:31 +0900

    chooser

--- javascripts/discourse/components/ttl-nav-item.js
+++ javascripts/discourse/components/ttl-nav-item.js
@@ -20,9 +20,7 @@ export default Component.extend({
     // let categoryText = document.querySelector(".ttl-nav-bar .nav-pills li.active a");
     if(this.active) {
       this.updateActiveNav();
-    } else {
-      // categoryText.style.color = "#646464";
-    }
+    } 
     // debugger
     // this.updateActiveNav();
       // this.updateActiveNav();
@@ -36,14 +34,14 @@ export default Component.extend({
 
 
   didInsertElement() {
-    if(this.active) {
-      this.updateActiveNav();
+    // if(this.active) {
+    //   this.updateActiveNav();
 
-    }
-    // const marker = document.querySelector('.ttl-nav-line');
-    // this.updateActiveNav( () => {
-    //     marker.style.visibility = "visible";
-    // });
+    // }
+    const marker = document.querySelector('.ttl-nav-line');
+    this.updateActiveNav( () => {
+        marker.style.visibility = "visible";
+    });
   },
     
     //    categoryAndTagUrl: function() {
@@ -52,55 +50,74 @@ export default Component.extend({
     //        console.log(categoryAndTagUrl);
     //        return categoryAndTagUrl;
     //     },
-  @discourseComputed("active")
-  getResult(active) {
-    // this.updateActiveNav();
-    if (active) {
-      this.updateActiveNav();
-      return true;
-    } else {
-      // return false;
-    }
-  },
+  // @discourseComputed("active")
+  // getResult(active) {
+  //   // this.updateActiveNav();
+  //   if (active) {
+  //     this.updateActiveNav();
+  //     return true;
+  //   } else {
+  //     // return false;
+  //   }
+  // },
 
-  @discourseComputed("active", "category.color")
-  updateActiveNav() {
-    // debugger
-    // console.log(this.active);
-    if(this.active) {
-      const selectedItem = document.querySelector('.active');
-      // console.log(selectedItem);
-      const scollLeft = document.querySelector('.nav-pills').scrollLeft;
-      const fontSize = document.defaultView.getComputedStyle(document.body, '').fontSize;
-      if(selectedItem) {
-          const halfRectWidth = selectedItem.getBoundingClientRect().width / 2;
-          const leftPosition =
-              selectedItem.getBoundingClientRect().left - selectedItem.parentNode.getBoundingClientRect().left + document.querySelector('.nav-pills').scrollLeft;
-          const marker = document.querySelector('.ttl-nav-line');
-          const categoryColor = this.category.color;
-          let categoryText = document.querySelector(".ttl-nav-bar .nav-pills li.active a");
-          let no_categoryText = document.querySelectorAll(".ttl-nav-bar .nav-pills li:not(.active) a");
+  // @discourseComputed("active", "category.color")
+  // updateActiveNav() {
+  //   // debugger
+  //   // console.log(this.active);
+  //   if(this.active) {
+  //     const selectedItem = document.querySelector('.active');
+  //     // console.log(selectedItem);
+  //     const scollLeft = document.querySelector('.nav-pills').scrollLeft;
+  //     const fontSize = document.defaultView.getComputedStyle(document.body, '').fontSize;
+  //     if(selectedItem) {
+  //         const halfRectWidth = selectedItem.getBoundingClientRect().width / 2;
+  //         const leftPosition =
+  //             selectedItem.getBoundingClientRect().left - selectedItem.parentNode.getBoundingClientRect().left + document.querySelector('.nav-pills').scrollLeft;
+  //         const marker = document.querySelector('.ttl-nav-line');
+  //         const categoryColor = this.category.color;
+  //         let categoryText = document.querySelector(".ttl-nav-bar .nav-pills li.active a");
+  //         let no_categoryText = document.querySelectorAll(".ttl-nav-bar .nav-pills li:not(.active) a");
   
-          marker.style.left = "".concat("calc(", leftPosition + halfRectWidth , "px", " - ", fontSize, ")");
-          marker.style.backgroundColor = "".concat("#",categoryColor);
-          // console.log(categoryColor);
+  //         marker.style.left = "".concat("calc(", leftPosition + halfRectWidth , "px", " - ", fontSize, ")");
+  //         marker.style.backgroundColor = "".concat("#",categoryColor);
+  //         // console.log(categoryColor);
           
-          categoryText.style.color = "".concat("#",categoryColor);
-          [].forEach.call(no_categoryText, function(t) {
-            // do whatever
-            t.style.color = "#646464";
-          });
-          // no_categoryText.style.color ="#646464";
-          // console.log(categoryColor);
-          //console.log(categoryText.style.color)
+  //         categoryText.style.color = "".concat("#",categoryColor);
+  //         [].forEach.call(no_categoryText, function(t) {
+  //           // do whatever
+  //           t.style.color = "#646464";
+  //         });
+  //         // no_categoryText.style.color ="#646464";
+  //         // console.log(categoryColor);
+  //         //console.log(categoryText.style.color)
 
-      }
-            // categoryText.style.color = "#646464" ;
-            // console.log("#646464");
+  //     }
+  //           // categoryText.style.color = "#646464" ;
+  //           // console.log("#646464");
           
-              // callback();
-    }
+  //             // callback();
+  //   }
+  // },
+  updateActiveNav(callback) {
+    const selectedItem = document.querySelector('.active');
+    const scollLeft = document.querySelector('.nav-pills').scrollLeft;
+    const fontSize = document.defaultView.getComputedStyle(document.body, '').fontSize;
+    if(selectedItem) {
+        const halfRectWidth = selectedItem.getBoundingClientRect().width / 2;
+        const leftPosition =
+            selectedItem.getBoundingClientRect().left - selectedItem.parentNode.getBoundingClientRect().left + document.querySelector('.nav-pills').scrollLeft;
+        const marker = document.querySelector('.ttl-nav-line');
+        if(this.router.currentRoute.attributes) {
+            const categoryColor = this.router.currentRoute.attributes.category.color;
+            const categoryText = document.querySelector(".ttl-nav-bar .nav-pills li.active a");
     
+            marker.style.left = "".concat("calc(", leftPosition + halfRectWidth , "px", " - ", fontSize, ")");
+            marker.style.backgroundColor = "".concat("#",categoryColor);
+            categoryText.style.color = "".concat("#",categoryColor);
+            callback();
+        }
+    }
   },
 
   @discourseComputed("category.isParent", "category.default_list_filter")
