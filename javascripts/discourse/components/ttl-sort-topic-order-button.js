4c5f8153537a2b8f0c575c41c840292254af6844
WJSirius1212 <u201911774@hust.edu.cn>
2022-08-01 20:22:54 +0800

    test new button

---
+++ javascripts/discourse/components/ttl-sort-topic-order-button.js
@@ -0,0 +1,17 @@
+import DButton from "discourse/components/d-button";
+
+export default DButton.extend({
+  pluginApiIdentifiers: ["ttl-sort-topic-order-button"],
+  classNames: ["ttl-sort-topic-order-button"],
+  click() {
+    const ascending = "?ascending=true&order=created";
+    const decending = "?order=created";
+
+    if (!window.location.search.test("ascending")) {
+        api.addDiscoveryQueryParam(decending, { replace: true, refreshModel: true });
+        this.classList.add("rotate")
+    } else {
+      api.addDiscoveryQueryParam(ascending, { replace: true, refreshModel: true });
+    }
+  },
+});
